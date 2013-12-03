b9f3942bebe688282c3f77dca9c949f8d9f0094e
Amal Hussein <amalhussein08@gmail.com>
2013-12-03 10:17:06 -0500

    created db and shuffle files around

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DesignMelodrama::Application.config.secret_key_base = '8dc6454c08995fdb4b1c319e04165b4318aa77dadd3047a3c6d5d56a7f8cee907e0a3cd0b27cdde42dc1314f66453c8e207c7111be0021cac4cba25a87b301fe'
